1c2bccff37e470e161eae9b3b7cf9bcdcad47819
Isaqueslv9 <isaquedzn9@gmail.com>
2026-04-24 16:57:17 -0300

    feat: v1.1 adiciona o case no monitoramento continuo

--- log_monitor/log_monitor.sh
+++ log_monitor/log_monitor.sh
@@ -3,16 +3,21 @@
 
 # Programa    : log_monitor.sh
 # Descrição   : Script para monitorar arquivos de log no Linux.
-# Uso         : sudo ./log_monitor.sh <arquivo_de_log> <palavra a ser procurada> <quantidade das ultiams linhas>
+# Uso         : sudo ./log_monitor.sh <arquivo_de_log> <palavra a ser procurada> <quantidade das ultimas linhas>
 # Autor       : Isaque S.
-# Versão      : 1.0
+# Versão      : 1.1
 # ==========================================
-
-if [ -n "$1" ]; then
-    arquivo="$1"
-else
-    arquivo="/var/log/messages"
+if [ -f "$1" ]; then 
+   echo "Arquivo de log que sera analisado: $1" 
+else 
+   echo "o arquivo informado nao existe, por favor informe um arquivo valido" && exit 1 
+fi 
+if [ -n "$1" ]; then 
+   arquivo="$1" 
+   else 
+   arquivo="/var/log/messages" 
 fi
+
 if [ -n "$2" ]; then
     palavra="$2"
 else
@@ -21,7 +26,7 @@ fi
 if [ -n "$3" ]; then
     linhas="$3"
 else
-    linhas="${linhas:-100}"
+    linhas="${3:-100}"
 fi
 
 echo "Iniciando o monitoramento do arquivo de log $arquivo por ocorrencias da palavra $palavra nas ultimas $linhas linhas."
@@ -37,19 +42,19 @@ echo "Deseja que continue monitorando o arquivo? (s/n)?"
 read -r resposta
 
 trap 'echo "Monitoramento finalizado pelo usuário."; exit 0' SIGINT
-
-if [[ "$resposta" =~ ^[sS]$ ]]; then
-    while true; do
+echo "Pressione Ctrl+C caso queira encerrar o monitoramento"
+case "$resposta" in
+    [sS]) while true; do
         sleep 10
         echo "verificando novamente o arquivo"
-
-        if grep -Ei "$palavra" "$arquivo" | tail -n "$linhas" > arquivo_de_errolog.txt; then
+        if grep -Ei "$palavra" "$arquivo" | tail -n "$linhas" >> arquivo_de_errolog.txt; then
             echo "Nova ocorrencias encontradas foram salvas no arquivo de erros."
         else
             echo "Nenhuma nova ocorrencia encontrada"
         fi 
-    done
-else
-    echo "Encerrando o monitoramento do arquivo de log."
-fi
+    done ;;
+    [nN])
+        echo "Encerrando o monitoramento do arquivo de log." && exit 0 ;;
+    *) echo "Resposta inválida. Encerrando o monitoramento do arquivo de log." && exit 1 ;;
+esac
 
