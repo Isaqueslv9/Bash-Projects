f011e220b634271b56361924a5c96d4c55da42d6
Isaque Sebastiao <isaquedzn9@gmail.com>
2026-04-30 10:18:21 -0300

    Adicionando cores no codigo.

--- check_service/check_service.sh
+++ check_service/check_service.sh
@@ -5,20 +5,22 @@
 # Descrição   : Script para verificar o status de um serviço no Linux. Caso esteja inativo, o script tentara iniciar o serviço.
 # Uso         : sudo ./check_service.sh <serviço>
 # Autor       : Isaque S.
-# Versão      : 1.0
+# Versão      : 1.1
 # ==========================================
+RED="\033[31m"
+GREEN="\033[32m"
 
 echo "Verificando o status do serviço $1..."
 
 if systemctl is-active --quiet $1; then
-    echo "O serviço $1 está ativo."
+    echo -e "${GREEN}O serviço $1 está ativo."
 else
     echo "O serviço $1 não está ativo. Vamos iniciá-lo..."
     systemctl start $1 --quiet
     systemctl start $1
         if systemctl is-active --quiet "$1"; then
-            echo "O serviço $1 foi iniciado com sucesso."
+           echo -e "${GREEN}O serviço $1 foi iniciado com sucesso"
         else
-            echo "Falha ao iniciar o serviço $1."
+            echo -e "${RED}Falha ao iniciar o serviço $1."
         fi
 fi
